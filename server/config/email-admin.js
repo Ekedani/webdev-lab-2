a9563f811e1c21120dd601cb64d40f60ce860459
Vladlen <ilusenkovladlen@gmail.com>
2021-10-07 19:45:21 +0300

    Started server creating

---
+++ server/config/email-admin.js
@@ -0,0 +1,3 @@
+const EMAIL_ADMIN_CONFIG = process.env.EMAIL_ADMIN
+
+export default EMAIL_ADMIN_CONFIG
---
+++ server/config/index.js
@@ -0,0 +1,4 @@
+export { default as EMAIL_USER_CONFIG } from './email-user.js'
+export { default as EMAIL_ADMIN_CONFIG } from './email-admin.js'
+export { default as EMAIL_CONFIG } from './email.js'
+export * from './limits.js'

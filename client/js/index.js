2aa3ce64ecbea0958555b4c9b50599206614d346
Vladlen <ilusenkovladlen@gmail.com>
2021-10-07 14:09:01 +0300

    Added spinner

---
+++ client/js/index.js
@@ -0,0 +1,23 @@
+const form = document.querySelector('.contact-form');
+const formSubmitButton = form.querySelector('button');
+const formMessage = form.querySelector('textarea');
+const contentBlock = document.querySelector('.content-block');
+
+const changeButtonState = (button, isActive) => {
+    button.disabled = !isActive;
+};
+
+const showBlock = (block) => {
+    block.style.visibility = 'visible';
+};
+
+const hideBlock = (block) => {
+    block.style.visibility = 'hidden';
+};
+
+const formSender = async () => {
+    changeButtonState(formSubmitButton, false);
+    showBlock(contentBlock);
+};
+
+formSubmitButton.addEventListener('click', formSender);

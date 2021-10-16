81f2fc10345d7ea42cc44a779a897f48efdad90f
Vladlen <ilusenkovladlen@gmail.com>
2021-10-16 14:39:27 +0300

    Minor change

--- server/config/limits.js
+++ server/config/limits.js
@@ -1,4 +1,6 @@
+// Allows user to send one additional request every minute even if cloud provider haven't terminated function instance
 const MINIMUM_REQUESTS_PAUSE = 60000;
+// Base request number for one user per work time of one function instance
 const MAXIMUM_REQUESTS_PER_SESSION = 10;
 
 export { MINIMUM_REQUESTS_PAUSE, MAXIMUM_REQUESTS_PER_SESSION };

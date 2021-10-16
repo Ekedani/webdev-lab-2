362237f9b2c4e870b6be4fa0f3980d551efc9cbe
Vladlen <ilusenkovladlen@gmail.com>
2021-10-16 13:56:50 +0300

    Added rate limit

--- server/api/messaging.js
+++ server/api/messaging.js
@@ -1,20 +1,25 @@
 import { MessageSender } from '../email-sender/index.js';
+import { UserRepository } from '../user-data/user-repository';
 
-const messageSender = new MessageSender();
+// For keeping information about user IP
+const userInformation = new UserRepository();
+const messageSender = new MessageSender(userInformation);
 
 export default (req, res) => {
-  try {
-    if (!req.body) {
-      throw new Error('Request is empty');
-    }
-    const formData = JSON.parse(req.body);
-    messageSender
-      .sendFormData(formData)
-      .then(res.send)
-      .catch(function (error) {
+    try {
+        const userIP =
+            req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+        if (!req.body) {
+            throw new Error('Request is empty');
+        }
+        const formData = JSON.parse(req.body);
+        messageSender
+            .sendFormData(formData, userIP)
+            .then(res.send)
+            .catch(function (error) {
+                res.send({ error: error.message });
+            });
+    } catch (error) {
         res.send({ error: error.message });
-      });
-  } catch (error) {
-    res.send({ error: error.message });
-  }
+    }
 };
--- server/email-sender/message-sender.js
+++ server/email-sender/message-sender.js
@@ -1,46 +1,69 @@
 import { EmailSender } from './index';
 import * as formProcessing from '../form-processing/index';
 import sanitizeHtml from 'sanitize-html';
+import {
+    MINIMUM_REQUESTS_PAUSE,
+    MAXIMUM_REQUESTS_PER_SESSION,
+} from '../config/index';
 
 export default class MessageSender {
-  constructor () {
-    this.emailSender = new EmailSender();
-  }
-
-  async sendFormData (formData) {
-    // Validating data received from the form
-    if (
-      !formProcessing.isValid(
-        formData.firstname,
-        formProcessing.NAME_REGEX
-      )
-    ) {
-      throw new Error('Invalid Firstname');
-    }
-    if (
-      !formProcessing.isValid(
-        formData.lastname,
-        formProcessing.NAME_REGEX
-      )
-    ) {
-      throw new Error('Invalid Lastname');
-    }
-    if (
-      !formProcessing.isValid(formData.email, formProcessing.EMAIL_REGEX)
-    ) {
-      throw new Error('Invalid Email');
+    constructor(userInformation) {
+        this.emailSender = new EmailSender();
+        this.userRepository = userInformation;
     }
 
-    // Generating and sanitizing HTML
-    const message = formProcessing.htmlConverter(formData);
-    const cleanMessage = sanitizeHtml(message);
+    async sendFormData(formData, userIP) {
+        // Check if user exceeded the request limit
+        let currentUser = this.userRepository.get(userIP);
+        if (!currentUser) {
+            this.userRepository.add(userIP, {
+                requests: 0,
+                lastRequestDate: new Date(),
+            });
+            currentUser = this.userRepository.get(userIP);
+        }
 
-    // Sending message
-    try {
-      await this.emailSender.send(cleanMessage, true);
-      return { message: 'Message was successfully sent' };
-    } catch (error) {
-      throw new Error('Sending error');
+        if (
+            currentUser.requests >= MAXIMUM_REQUESTS_PER_SESSION &&
+            new Date() - new Date(currentUser.lastRequestDate) <
+            MINIMUM_REQUESTS_PAUSE
+        ) {
+            throw new Error('You have exceeded the request limit');
+        }
+
+        // Validating data received from the form
+        if (
+            !formProcessing.isValid(
+                formData.firstname,
+                formProcessing.NAME_REGEX
+            )
+        ) {
+            throw new Error('Invalid first name');
+        }
+        if (
+            !formProcessing.isValid(
+                formData.lastname,
+                formProcessing.NAME_REGEX
+            )
+        ) {
+            throw new Error('Invalid last name');
+        }
+        if (
+            !formProcessing.isValid(formData.email, formProcessing.EMAIL_REGEX)
+        ) {
+            throw new Error('Invalid email');
+        }
+
+        // Generating and sanitizing HTML
+        const message = formProcessing.htmlConverter(formData);
+        const cleanMessage = sanitizeHtml(message);
+
+        // Sending message
+        try {
+            await this.emailSender.send(cleanMessage, true);
+            return { message: 'Message was successfully sent' };
+        } catch (error) {
+            throw new Error('Sending error');
+        }
     }
-  }
 }

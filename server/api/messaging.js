92e3df08bde92452c4ea3ba7185d8360ed1ef197
Vladlen <ilusenkovladlen@gmail.com>
2022-01-06 16:33:25 +0200

    Moved rate limit from send to messaging.js

--- server/api/messaging.js
+++ server/api/messaging.js
@@ -1,19 +1,50 @@
 import { MessageSender } from '../email-sender/index.js';
 import { UserRepository } from '../user-data/user-repository';
 import { ApiError } from '../form-processing/ApiError';
+import {
+    MAXIMUM_REQUESTS_PER_SESSION,
+    MINIMUM_REQUESTS_PAUSE
+} from '../config';
 
 // For keeping information about user IP
 const userInformation = new UserRepository();
 const messageSender = new MessageSender(userInformation);
 
 export default (req, res) => {
+    const userIP =
+        (req.headers['x-forwarded-for'] || '').split(',')[0] ||
+        req.connection.remoteAddress;
+
+    const currentDate = new Date();
+    // Check if user exceeded the request limit
+    let currentUser = userInformation.get(userIP);
+    if (!currentUser) {
+        this.userRepository.add(userIP, {
+            requests: 0,
+            lastRequestDate: currentDate
+        });
+        currentUser = this.userRepository.get(userIP);
+    }
+
     try {
-        const userIP =
-            (req.headers['x-forwarded-for'] || '').split(',')[0] ||
-            req.connection.remoteAddress;
         if (!req.body) {
-            throw new ApiError('Request is empty');
+            throw new ApiError('Request is empty', 204);
         }
+
+        if (
+            currentUser.requests >= MAXIMUM_REQUESTS_PER_SESSION &&
+            currentDate - new Date(currentUser.lastRequestDate) <
+                MINIMUM_REQUESTS_PAUSE
+        ) {
+            throw new ApiError('You have exceeded the request limit', 429);
+        }
+
+        // Update information about user in repo
+        this.userRepository.add(userIP, {
+            requests: currentUser.requests + 1,
+            lastRequestDate: currentDate
+        });
+
         const formData = JSON.parse(req.body);
         messageSender
             .sendFormData(formData, userIP)
--- server/email-sender/MessageSender.js
+++ server/email-sender/MessageSender.js
@@ -2,10 +2,6 @@ import { EmailSender } from './index';
 import { ApiError } from '../form-processing/ApiError';
 import * as formProcessing from '../form-processing/index';
 import sanitizeHtml from 'sanitize-html';
-import {
-    MINIMUM_REQUESTS_PAUSE,
-    MAXIMUM_REQUESTS_PER_SESSION
-} from '../config/index';
 
 export default class MessageSender {
     constructor(userInformation) {
@@ -14,31 +10,6 @@ export default class MessageSender {
     }
 
     async sendFormData(formData, userIP) {
-        // Check if user exceeded the request limit
-        let currentUser = this.userRepository.get(userIP);
-        if (!currentUser) {
-            this.userRepository.add(userIP, {
-                requests: 0,
-                lastRequestDate: new Date()
-            });
-            currentUser = this.userRepository.get(userIP);
-        }
-
-        const currentDate = new Date();
-        if (
-            currentUser.requests >= MAXIMUM_REQUESTS_PER_SESSION &&
-            currentDate - new Date(currentUser.lastRequestDate) <
-                MINIMUM_REQUESTS_PAUSE
-        ) {
-            throw new ApiError('You have exceeded the request limit', 429);
-        }
-
-        // Update information about user in repo
-        this.userRepository.add(userIP, {
-            requests: currentUser.requests + 1,
-            lastRequestDate: currentDate
-        });
-
         // Validating data received from the form
         if (
             !formProcessing.isValid(
